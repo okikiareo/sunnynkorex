a9332f8cf5fdc1a66b45a74263f9f0a14f372fb7
unknown <okikiareo@gmail.com>
2024-01-15 19:24:25 -0800

    push

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ const style = {
     hide: "hidden",
     show: "auto"
 }
+
+// Navbar
+const toggleHeader = () => {
+    if (navbarMenu.classList.contains("open")) {
+        document.body.style.setProperty("overflow", style.show);
+    } else {
+        document.body.style.setProperty("overflow", style.hide);
+    }
+    navbarMenu.classList.toggle("open");
+    headerMenu.classList.toggle("open");
+}
+
+headerMenu.addEventListener("click", () => toggleHeader());
